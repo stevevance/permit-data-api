2c5800ab83b238050f867212e52bd80e2c92bca5
Mark Headd <mheadd@gmail.com>
2015-04-15 10:09:42 -0400

    unfixed variable declarations

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 exports.config = config = {
 	// Port for Express app to listen on.
-	var port: process.argv[2] || 3000,
+	port: process.argv[2] || 3000,
 
 	// Default limit for records to fetch
-	var LIMIT: process.argv[3] || 25,
+	LIMIT: process.argv[3] || 25,
 
 	// CKAN API base.
-	var CKAN_API_BASE: 'http://www.civicdata.com/api/action/datastore_search_sql?sql=',
+	CKAN_API_BASE: 'http://www.civicdata.com/api/action/datastore_search_sql?sql=',
 
 	// Resource IDs.
-	var PERMITS: "03b30af0-1ef8-4b6c-bc59-859fd8e19e5c",
-	var PERMIT_HISTORY: "87f54b3e-07ad-4595-b78f-e748071d6f18",
-	var INSPECTIONS: "f40902f2-3f3e-4aa2-9326-0f88d6d07ae4",
-	var CONTRACTORS: "5aed9714-8798-4948-9e58-c8a76d5fae61"
+	PERMITS: "03b30af0-1ef8-4b6c-bc59-859fd8e19e5c",
+	PERMIT_HISTORY: "87f54b3e-07ad-4595-b78f-e748071d6f18",
+	INSPECTIONS: "f40902f2-3f3e-4aa2-9326-0f88d6d07ae4",
+	CONTRACTORS: "5aed9714-8798-4948-9e58-c8a76d5fae61"
 }
