a376c7701b5f8455a8c689de96f4eb90ac751206
Mark Headd <mheadd@gmail.com>
2015-04-15 11:45:38 -0400

    Modified order of invocation params

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
 exports.config = config = {
-	// Port for Express app to listen on.
-	port: process.argv[2] || 3000,
 
 	// Default limit for records to fetch
-	LIMIT: process.argv[3] || 25,
+	LIMIT: process.argv[2] || 25,
+
+	// Port for Express app to listen on.
+	port: process.argv[3] || 3000,
 
 	// CKAN API base.
 	CKAN_API_BASE: 'http://www.civicdata.com/api/action/datastore_search_sql?sql=',
 
-	// Resource IDs.
+	// Jurisdictions
 	PERMITS: "03b30af0-1ef8-4b6c-bc59-859fd8e19e5c",
 	PERMIT_HISTORY: "87f54b3e-07ad-4595-b78f-e748071d6f18",
 	INSPECTIONS: "f40902f2-3f3e-4aa2-9326-0f88d6d07ae4",
 	CONTRACTORS: "5aed9714-8798-4948-9e58-c8a76d5fae61"
+
 }
